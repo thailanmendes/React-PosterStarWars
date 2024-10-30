3d0b92f93de1f3afa291c656fbbb6eaf1a346b88
Thailan Mendes de Lima <101593899+thailanmendes@users.noreply.github.com>
2024-10-30 10:23:22 -0300

    adicionando o componente Button

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import posterImg from "../../assets/poster.jpg"
+import Button from "../Button"
 import styles from "./styles.module.css"
 
 export default function Card() {
@@ -9,7 +10,7 @@ export default function Card() {
         <h2 className={styles.title}>Pôster do filme Star Wars</h2>
         <p className={styles.description}>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
         <p></p>
-        <button className={styles.button}>Comprar agora</button>
+        <Button/>
       </div>
     </div>
   )
